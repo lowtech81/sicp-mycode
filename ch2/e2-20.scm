877b8526fc708af451f69ce868235dd00f93c3cb
Kaijin Lyu <kaijin.lv@gmail.com>
2019-11-04 09:32:57 +0800

    finished e2-20

---
+++ ch2/e2-20.scm
@@ -0,0 +1,58 @@
+; Exercise 2.20.
+;
+; The procedures +, *, and list take arbitrary numbers of arguments. One way to
+; define such procedures is to use define with 'dotted-tail notation'. In a procedure
+; definition, a parameter list that has a dot before the last parameter name
+; indicates that, when the procedure is called, the initial parameters (if any) will
+; have as values the initial arguments, as usual, but the final parameter's value
+; will be a list of any remaining arguments. For instance, given the definition
+;
+; (define (f x y . z) <body>)
+;
+; the procedure f can be called with two or more arguments. If we evaluate
+
+; (f 1 2 3 4 5 6)
+
+; then in the body of f, x will be 1, y will be 2, and z will be the list (3 4 5 6). Given the definition
+
+; (define (g . w) <body>)
+
+; the procedure g can be called with zero or more arguments. If we evaluate
+
+; (g 1 2 3 4 5 6)
+
+; then in the body of g, w will be the list (1 2 3 4 5 6).
+
+; Use this notation to write a procedure same-parity that takes one or more integers and
+; returns a list of all the arguments that have the same even-odd parity as the first argument. For example,
+
+; (same-parity 1 2 3 4 5 6 7)
+; (1 3 5 7)
+
+; (same-parity 2 3 4 5 6 7)
+; (2 4 6)
+; ---------------------------------------------------------------------------------------------------- 
+
+; concat two lists
+(define (append list1 list2)
+    (if (null? list1)
+        list2
+        (cons (car list1) (append (cdr list1) list2))))
+
+(define (same-p? a b)
+    (= (remainder a 2) (remainder b 2)))
+
+(define (same-parity first . rest)
+    (define (same-parity-iter result left)
+            (if (null? left)
+                result
+                (if (same-p? first (car left))
+                    (same-parity-iter (append result (list (car left))) (cdr left))
+                    (same-parity-iter result (cdr left)))
+                )
+        )
+    (same-parity-iter (list first) rest)
+    )
+
+(same-parity 1 2 3 4 5 6 7)
+(same-parity 2 3 4 5 6 7)
